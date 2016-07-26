581cb3b60fc779e222e49b7e434540651c11ea85
stan <feihu_guest@qq.com>
2016-07-26 09:38:05 +0800

    Enhanced IVR_install.bat and IVR_uninstall.bat to adapter windows10.

--- attachment/sdk/IVR_install.bat
+++ attachment/sdk/IVR_install.bat
@@ -1,8 +1,13 @@
+cd /d "%~dp0"
+
 ::x64
 copy "x64\libIVR.dll" "%SystemRoot%\system32"
-
+copy "x64\libIVRSDK_1.dll" "%SystemRoot%\system32"
 
 ::x86
 copy "x86\libIVR.dll" "%SystemRoot%\syswow64"
+copy "x86\libIVRSDK_1.dll" "%SystemRoot%\syswow64"
+
+pause
 
 
